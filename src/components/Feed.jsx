33b32bf9e2356714780d7ef65b8a9963dbe28370
lex62510 <eagle625@tut.by>
2024-04-16 22:25:35 +0300

    add feed and MiniProfile component

---
+++ src/components/Feed.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Posts from "@/components/Posts";
+import MiniProfile from "@/components/MiniProfile";
+
+const Feed = () => {
+    return (
+        <main className={'grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'}>
+            <section className={'md:col-span-2'}>
+                <Posts/>
+            </section>
+            <section className={'hidden md:inline-grid md:col-span-1'}>
+                <MiniProfile/>
+            </section>
+        </main>
+    );
+};
+
+export default Feed;
